78802dcd3ef227982e4f287c60c97b04d884ebf5
Jeus Jurado <ijurado@acsyt.com>
2023-10-18 15:53:22 -0600

    config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+// module.exports = {
+//   // devtool: '',
+//   entry: {
+//     'admin-ranking-table': './src/js/admin-ranking-table.js',
+//   },
+//   output: {
+//     filename: '[name].js',
+//     path: path.resolve(__dirname, 'js'),
+//   }
+// };
