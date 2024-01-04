0ed433ac1554836af5435d4af6aeab86286c3b85
Jeus Jurado <ijurado@acsyt.com>
2024-01-04 09:38:15 -0600

    v1.0.10

--- muguerza-bookings.php
+++ muguerza-bookings.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Muguerza Bookings
  * Description: Muguerza Bookings.
- * Version: 1.0.9
+ * Version: 1.0.10
  * Author: Acsyt
  * Author URI: http://acsyt.com
  * Developer: Acsyt
