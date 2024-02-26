cceffa6621537139b5ab6ae84d39d53d1f8a4d08
Jeus Jurado <ijurado@acsyt.com>
2024-02-26 14:58:18 -0600

    fix: add required apex api params

--- inc/api/class-mg-apex-appt-item.php
+++ inc/api/class-mg-apex-appt-item.php
@@ -14,6 +14,8 @@ class MG_Apex_Appt_Item {
     public $p_phone;
     public $p_age;
     public $p_confirm;
+    public $p_user; // Solicitado por el equipo de APEX
+    public $p_lattribute1; // Product name
 
     public static function from_booking( MG_Booking $booking ) {
         $apex_item = new self();
@@ -33,6 +35,8 @@ class MG_Apex_Appt_Item {
         $apex_item->p_genre           = $booking->get_sex();
         $apex_item->p_phone           = $booking->get_phone();
         // $apex_item->p_age             = $booking->get_age();
+        $apex_item->p_user            = 'api'; // Solicitado por el equipo de APEX
+        $apex_item->p_lattribute1     = get_the_title( $booking->get_product_id() );
 
         return $apex_item;
     }
@@ -41,31 +45,7 @@ class MG_Apex_Appt_Item {
         return new self( $data );
     }
 
-    public function __construct( array $data = array() ) {
-
-        $_body = array(
-            'p_email'           => 'ARTURO.SALAS@CHRISTUS.MX',
-            'p_dayweek'         => '4',
-            // 'p_user'            => 'api',
-            // 'p_confirm'          => 'Y',
-            // 'p_pacient_name'    => 'arturo salas',
-            'p_calendar'        => '51',
-            // 'p_date_finish'      => '2023-07-02T12:00:00',
-            // 'p_unit'            => '1',
-            // 'p_phone'           => '8127325334',
-            'p_date_start'      => '2023-07-04T11:00:00',
-            // 'p_comments'        => '',
-            // 'p_title'           => '10:00 AM a 11:00 PM-CITA: ARTURO SALAS FLORES',
-            // 'p_curp'            => 'SAFA92',
-            // 'p_state'           => 'NL',
-            // 'p_birth_date'      => '',
-            // 'p_genre'           => '',
-            // 'p_second_lastname' => '',
-            // 'p_first_lastname'   => '',
-            // 'p_middle_name'     => '',
-            // 'p_first_name'       => '',
-        );
-        
+    public function __construct( array $data = array() ) {        
         $data = wp_parse_args( $data, array(
             'p_email'           => '',
             'p_calendar'        => '',
@@ -73,13 +53,15 @@ class MG_Apex_Appt_Item {
             'p_curp'            => '',
             'p_state'           => '',
             'p_birth_date'      => '',
-            'p_first_name'       => '',
-            'p_first_lastname'   => '',
+            'p_first_name'      => '',
+            'p_first_lastname'  => '',
             'p_second_lastname' => '',
             'p_genre'           => '',
             'p_phone'           => '',
             'p_age'             => '',
-            'p_confirm'          => 'P',
+            'p_confirm'         => 'P',
+            'p_user'            => 'api',
+            'p_lattribute1'     => '',
         ) );
 
         $this->p_email           = $data['p_email'];
@@ -88,13 +70,15 @@ class MG_Apex_Appt_Item {
         $this->p_curp            = $data['p_curp'];
         $this->p_state           = $data['p_state'];
         $this->p_birth_date      = $data['p_birth_date'];
-        $this->p_first_name       = $data['p_first_name'];
-        $this->p_first_lastname   = $data['p_first_lastname'];
+        $this->p_first_name      = $data['p_first_name'];
+        $this->p_first_lastname  = $data['p_first_lastname'];
         $this->p_second_lastname = $data['p_second_lastname'];
         $this->p_genre           = $data['p_genre'];
         $this->p_phone           = $data['p_phone'];
         $this->p_age             = $data['p_age'];
-        $this->p_confirm          = $data['p_confirm'];
+        $this->p_user            = $data['p_user'];
+        $this->p_lattribute1     = $data['p_lattribute1'];
+        $this->p_confirm         = $data['p_confirm'];
     }
 
     public function get_data() {
